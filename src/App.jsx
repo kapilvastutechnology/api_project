a3a424dfc717d497d2e3b5260a098a676aa2689a
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2025-10-06 07:34:54 +0545

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import { createBrowserRouter } from "react-router"
 import RootLayout from "./components/RootLayout"
 import { RouterProvider } from "react-router-dom"
-import Home from "./home/Home"
-import UserEdit from "./users/UserEdit"
-import UserAdd from "./users/UserAdd"
+import Home from "./features/home/Home"
+
 export default function App() {
-
-  const users = [
-    {id:1, name:'ram'},
-    {id:2, name:'hari'}
-  ];
-
-  const newUser = users.map((user)=>{
-    return user.id===1?{id:1, name:'hari'}:user;
-  });
-  console.log(newUser);
 
   const router = createBrowserRouter([
     { 
@@ -25,16 +14,7 @@ export default function App() {
           index:true,
           element:<Home/>
         },
-        {
-          path:'add-user',
-          element:<UserAdd/>
-        },
         
-        {
-          path: 'edit-user/:id',
-          element: <UserEdit />
-        },
-
       ]
     }
   ])
--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { userSlice } from "../users/userSlice";
 
 export const store = configureStore({
     reducer:{
 
-        userSlice:userSlice.reducer
     }
 })
