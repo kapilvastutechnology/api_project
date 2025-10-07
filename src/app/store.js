32647530286577c20796bd537cc4f09151ebf64f
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2025-10-07 09:17:25 +0545

    update

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
+// import { cocktailApi } from "../features/cocktails/CocktailApi";
+// import { getDefaultValueType } from "framer-motion";
+// import { ApiProvider } from "@reduxjs/toolkit/query/react";
 
-export const store = configureStore({
-    reducer:{
-
-    }
-})
+// export const store = configureStore({
+//     reducer:{
+//         cocktailApi:cocktailApi.reducer
+//     },
+//     // caching, invalidation, polling
+//     middleware: (getDefaultValueType)=>
+//         getDefaultValueType().concat([
+//             cocktailApi.middleware
+//         ]),
+    
+// })
---
+++ src/features/cocktailApi/CocktailApi.js
@@ -0,0 +1,10 @@
+
+
+
+import { createApi } from '@reduxjs/toolkit/query/react'
+
+export const pokemonApi = createApi({
+  reducerPath: 'pokemonApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' })
+ 
+  })
